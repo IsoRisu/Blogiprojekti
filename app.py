c4c76614e0570e590ff8ce2acd5559a122a1d71e
IsoRisu <kraastian2@gmail.com>
2024-04-07 05:04:53 +0300

    FIrst testing phase

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-"Tes
+from flask import Flask, render_template
+from os import getenv
+
+
+posts = [
+    {   "author": "Risu",
+        "title": "Ensimmainen blogini",
+        "content": "Tama on ensimmainen kirjoitukseni.",
+        "date_posted": "7.4.2024"
+    },
+    {   "author": "Kisu",
+        "title": "Hellurei",
+        "content": "Moi kaikille.",
+        "date_posted": "6.4.2024"
+    }
+]
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("home.html", posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
