0616509d8eb58e78ea47ae1f19dcc7a32d7d5ed7
IsoRisu <kraastian2@gmail.com>
2024-04-21 22:27:03 +0300

    Blog table added to database vote table added to database without further functionality

--- schema.sql
+++ schema.sql
@@ -1,6 +1,22 @@
 -- Users table
 CREATE TABLE public.users (
     id serial primary key,
-    username text,
+    username text unique,
     password text
 );
+
+CREATE TABLE public.blogs (
+    id serial primary key,
+    author text,
+    title text,
+    content text,
+    date_posted timestamp
+    
+);
+
+CREATE TABLE public.votes (
+    id serial primary key,
+    vote integer
+);
+
+
